a03bde999b2e96966b62db4d983013ee8f0c7c35
MorxTheGamer <zamarwint11@gmail.com>
2022-03-28 16:41:07 +0000

    Changes with Java

--- Java/App.java
+++ Java/App.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
 import java.util.*;
 
 public class App {
   public ArrayList<String> people = new ArrayList<>();
-  public Data myData;
+  public Data jsonData = new Data("../../DataBase/java.json");
+  public Data chatData = new Data("../../chat.yaml");
+  public HashMap<String, String> messages = new HashMap<>();
+  public int count = -1;
+  public int msgCount = 0;
   public Scanner scan;
-  public String infoFile = "../DataBase/java.json";
 
   public void getInfo() {
     scan = new Scanner(System.in);
-    System.out.println("Enter the amount of people you want to communicate with: >");
-    String[] persons = scan.next().split(" ");
-    int count = -1;
+    System.out.print("Enter the amount of people you want to communicate with:\n> ");
+    String[] persons = scan.nextLine().split(" ");
 
     for (String person : persons) {
       count++;
@@ -19,15 +20,39 @@ public class App {
     }
   }
 
-  public void start() {
-    getInfo();
-    appLoop();
+  public void firstLoop() {
+    scan = new Scanner(System.in);
+    String[] data = jsonData.getData();
+    for (String d : data) {
+      count++;
+      people.add(count, d);
+    }
+    while (true) {
+      msgCount++;
+
+      System.out.print("Enter a person:\n> ");
+      String personInput = scan.nextLine();
+
+      if (personInput.equals(".")) break;
+      if (!people.contains(personInput)) {
+        System.out.println("Invalid Input.");
+        continue;
+      }
+
+      System.out.print("Type a message:\n> ");
+      String message = scan.nextLine();
+      String person = people.get(people.indexOf(personInput));
+      System.out.println(person + ": " + message);
+
+      messages.put(msgCount + person, message);
+    }
   }
 
-  public void appLoop() {
+  public void secondLoop() {
+    getInfo();
     scan = new Scanner(System.in);
     while (true) {
-      System.out.println("Choose a person: ");
+      System.out.print("Choose a person: ");
       String personInput = scan.nextLine();
 
       if (personInput.equals(".")) break;
@@ -37,14 +62,41 @@ public class App {
         continue;
       }
 
-      System.out.println("Type a message:\n>");
+      System.out.print("Type a message:\n> ");
       String message = scan.nextLine();
-      System.out.println(people.get(people.indexOf(personInput)) + ": " + message);
+      String person = people.get(people.indexOf(personInput));
+      System.out.println(person + ": " + message);
     }
   }
 
+  public void appLoop() {
+    scan = new Scanner(System.in);
+    while (true) {
+      System.out.print("Would you like to use the existing people or create new ones? (1/2):\n> ");
+      String option = scan.nextLine();
+
+      if (option.equals("1")) {
+        firstLoop();
+        break;
+      }
+
+      if (option.equals("2")) {
+        secondLoop();
+        break;
+      }
+
+      if (!option.equals("1") || !option.equals("2")) {
+        System.out.println("Invalid choice.");
+        continue;
+      }
+    }
+  }
+
+  public void start() {
+    appLoop();
+  }
+
   public void saveInfo() {
-    myData = new Data(infoFile);
-    myData.setData(people.toString());
+    jsonData.setData(people.toString());
   }
 }
--- Java/Data.java
+++ Java/Data.java
@@ -1,33 +1,37 @@
 import java.util.Scanner;
-import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 public class Data {
   public String file;
   public Scanner scan;
-  public BufferedWriter writer;
-  public FileWriter write;
+  public FileWriter writer;
+  public int count = -1;
+  public String[] lines;
 
   public Data(String file) {
     this.file = file;
   }
 
-  public void getData() {
+  public String[] getData() {
     try {
       scan = new Scanner(file);
-      while (scan.nextLine() != null) {
-        System.out.println(scan.nextLine());
+      while (scan.hasNextLine()) {
+        count++;
+        lines[count] = scan.nextLine();
       }
     } catch (Exception e) {
       e.printStackTrace();
     }
+    return lines;
   }
 
   public void setData(String data) {
     try {
-      writer = new BufferedWriter(new FileWriter(file));
+      writer = new FileWriter(file);
       System.out.println("Setting Data: " + data);
       writer.write(data);
+      writer.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
