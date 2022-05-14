cd1841434a9fb2bf21cdcb4dc169644941d2a334
MorxTheGamer <zamarwint11@gmail.com>
2022-05-14 18:43:39 -0500

    Even More

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app import App
 from tkinter import *
+from threading import Thread
 
 class Main:
     def __init__(self):
@@ -8,6 +9,14 @@ class Main:
         self.window.geometry('400x400')
         self.window.config(bg='black')
         self.window.title('Chat App')
+        
+        self.chat_label = Label(
+            self.window,
+            text="Welcome to Chat App!",
+            font=("Courier", 14, 'bold'),
+            bg='black',
+            fg='white'
+        )
 
         self.sign_up_button = Button(
             self.window,
@@ -55,9 +64,15 @@ class Main:
         )
 
     def start(self):
+        self.chat_label.place(x=90, y=60)
+        self.sign_up_button.place(x=150, y=150)
+        self.login_button.place(x=155, y=190)
+        self.status_button.place(x=155, y=230)
+        self.change_button.place(x=140, y=270)
+        self.delete_button.place(x=115, y=310)
+
         self.window.mainloop()
 
-    
-
 if __name__ == "__main__":
-    main()
+    app = Main()
+    app.start()
