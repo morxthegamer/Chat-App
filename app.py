1b8ef32d2367bd40187561930cfbf88f07fa1437
MorxTheGamer <zamarwint11@gmail.com>
2022-05-23 22:28:18 -0500

    Done Lots Of Work

--- app.py
+++ app.py
@@ -10,18 +10,21 @@ class App:
         self.data = Data('DataBase')
 
     def status(self):
-        self.status_wind = Tk()
-        self.status_wind.geometry('400x400')
+        i = self.login()
 
-        self.status_lbl = Label(
-            self.status_wind,
+        status_wind = Tk()
+        status_wind.geometry('400x400')
+        status_wind.config(bg='black')
+        status_wind.title('Status')
+
+        status_lbl = Label(
+            status_wind,
             text='Status',
             font=('Times', 20, 'bold')
         )
 
-        i = self.login()
-        self.status_text = Label(
-            self.status_wind,
+        status_text = Label(
+            status_wind,
             text=f"""
                 Username: {i['Username']}
                 Email: {i['Email']}
@@ -37,70 +40,61 @@ class App:
             font=('Courier', 20, 'bold')
         )
 
-    def login(self):            
-        self.login_wind = Tk()
-        self.login_wind.geometry('400x400')
-        self.login_wind.config(bg='black')
+        status_lbl.place(x=110, y=160)
+        status_text.place(x=110, y=200)
 
-        self.login_label = Label(
-            self.login_wind,
+        status_wind.mainloop()
+
+    def login(self):            
+        login_wind = Tk()
+        login_wind.geometry('400x400')
+        login_wind.config(bg='black')
+        login_wind.title('Log In')
+
+        login_label = Label(
+            login_wind,
             text='Please Login',
             font=('Courier', 20,'bold'),
             bg='black',
             fg='white'
         )
 
-        self.name_label = Label(
-            self.login_wind,
+        name_label = Label(
+            login_wind,
             text='Username:',
             font=('Courier', 10, 'bold'),
             bg='black',
             fg='white'
         )
 
-        self.pass_label = Label(
-            self.login_wind,
+        pass_label = Label(
+            login_wind,
             text='Password:',
             font=('Courier', 10, 'bold'),
             bg='black',
             fg='white'
         )
 
-        self.username = Entry(
-            self.login_wind,
-            textvariable=self.name_label,
+        username = Entry(
+            login_wind,
+            textvariable=name_label,
             width=30,
         )
 
-        self.password = Entry(
-            self.login_wind,
-            textvariable=self.pass_label,
+        password = Entry(
+            login_wind,
+            textvariable=pass_label,
             width=30,
         )
 
-        self.confirm_button = Button(
-            self.login_wind,
-            text='Enter',
-            font=('Courier', 12, 'bold'),
-            bg='black',
-            fg='white'
-            command=get_info
-        )
-        
-        self.login_label.place(x=100, y=60)
-        self.name_label.place(x=65, y=140)
-        self.pass_label.place(x=65, y=160)
-        self.username.place(x=145, y=140)
-        self.password.place(x=145, y=160)
-        self.confirm_button.place(x=165, y=200)
-
-        name, pw = self.username.get('1.0', 'end'), self.password.get()
-
-        def get_info(self):
+        def get_info():
+            name, pw = username.get(), password.get()
+            login_wind.destroy()
+            
             print('Logging in...\n')
             time.sleep(2)
 
-            i = self.data.getDataJson(f"user[{self.username.get('1.0', 'end')}].json")
+            i = self.data.getDataJson(f"user[{name}].json")
 
             if (i["Username"] != name):
                 print("Login failed. Wrong Username. Please try again.")
@@ -115,53 +109,173 @@ class App:
                 
             return i
 
-        self.login_wind.mainloop()
+        confirm_button = Button(
+            login_wind,
+            text='Enter',
+            font=('Courier', 12, 'bold'),
+            bg='black',
+            fg='white',
+            command=get_info
+        )
+        
+        login_label.place(x=100, y=60)
+        name_label.place(x=65, y=140)
+        pass_label.place(x=65, y=160)
+        username.place(x=145, y=140)
+        password.place(x=145, y=160)
+        confirm_button.place(x=165, y=200)
+
+        login_wind.mainloop()
 
     def sign_up(self):
-        os.system('cls')
-        username = input("Please enter your username: ")
-        email = input("Please enter your email: ")
-        password = input("Please enter you password: ")
+        sign_up_wind = Tk()
+        sign_up_wind.geometry('400x400')
+        sign_up_wind.config(bg='black')
+        sign_up_wind.title('Sign Up')
 
-        for file in os.listdir('DataBase'):
-            filename = file[5:-6]
+        # Labels
 
-            if username == filename:
-                print('This username is already taken. Please try again.')
-                exit(1)
+        sign_up_lbl = Label(
+            sign_up_wind,
+            text='Sign Up',
+            font=('Courier', 20, 'bold'),
+            bg='black',
+            fg='white'
+        )
 
-            mail = self.data.getDataJson(file)['Email']
+        name_label = Label(
+            sign_up_wind,
+            text='Username:',
+            font=('Courier', 10, 'bold'),
+            bg='black',
+            fg='white'
+        )
 
-            if (email == mail):
-                print('This email is already taken. Please try again.')
-                exit(1)
+        mail_label = Label(
+            sign_up_wind,
+            text='Email:',
+            font=('Courier', 10, 'bold'),
+            bg='black',
+            fg='white'
+        )
 
-        os.system('cls')
-        age = input('Please enter your age: ')
-        phone_number = input('Please enter your phone number: ')
+        pass_label = Label(
+            sign_up_wind,
+            text='Password:',
+            font=('Courier', 10, 'bold'),
+            bg='black',
+            fg='white'
+        )
 
-        user = {
-            'Username': username,
-            'Email': email,
-            'Password': password,
-            'Age': age,
-            'Contact': phone_number,
-            'Extras': {
-                'Theme': 'white',
-                'Badge': '💬',
-                'Text': 'Type A Message: ',
-                'Boost Subscription': False
+        age_label = Label(
+            sign_up_wind,
+            text='Age:',
+            font=('Courier', 10, 'bold'),
+            bg='black',
+            fg='white'
+        )
+
+        phone_number_label = Label(
+            sign_up_wind,
+            text='Phone Number:',
+            font=('Courier', 10, 'bold'),
+            bg='black',
+            fg='white'
+        )
+
+        # Text Area
+
+        username = Entry(
+            sign_up_wind,
+            textvariable=name_label,
+            width=30,
+        )
+
+        email = Entry(
+            sign_up_wind,
+            textvariable=mail_label,
+            width=30,
+        )
+
+        password = Entry(
+            sign_up_wind,
+            textvariable=pass_label,
+            width=30,
+        )
+
+        age = Entry(
+            sign_up_wind,
+            textvariable=age_label,
+            width=30,
+        )
+
+        phone_number = Entry(
+            sign_up_wind,
+            textvariable=phone_number_label,
+            width=30,
+        )
+
+        def set_data():
+            name, mail, pw, age, number = username.get(), email.get(), password.get(), age.get(), phone_number.get()
+            for file in os.listdir('DataBase'):
+                filename = file[5:-6]
+
+                if name == filename:
+                    print('This username is already taken. Please try again.')
+                    exit(1)
+
+                mail = self.data.getDataJson(file)['Email']
+
+                if (email == mail):
+                    print('This email is already taken. Please try again.')
+                    exit(1)
+
+            user = {
+                'Username': name,
+                'Email': mail,
+                'Password': pw,
+                'Age': age,
+                'Contact': number,
+                'Extras': {
+                    'Theme': 'white',
+                    'Badge': '💬',
+                    'Text': 'Type A Message: ',
+                    'Boost Subscription': False
+                }
             }
-        }
 
-        print('Signing you up...')
-        time.sleep(2)
+            print('Signing you up...')
+            time.sleep(2)
 
-        self.data.setDataJson(f'user[{username}].json', user)
-        print(f"Sign up was successful! Welcome {username}.")
+            self.data.setDataJson(f'user[{name}].json', user)
+            print(f"Sign up was successful! Welcome {name}.")
+
+        confirm_button = Button(
+            sign_up_wind,
+            text='Enter',
+            font=('Courier', 12, 'bold'),
+            bg='black',
+            fg='white',
+            command=set_data
+        )
+
+        sign_up_lbl.place(x=143, y=60)
+        name_label.place(x=65, y=140)
+        mail_label.place(x=89, y=160)
+        pass_label.place(x=65, y=180)
+        age_label.place(x=105, y=200)
+        phone_number_label.place(x=33, y=220)
+
+        username.place(x=145, y=140)
+        email.place(x=145, y=160)
+        password.place(x=145, y=180)
+        age.place(x=145, y=200)
+        phone_number.place(x=145, y=220)
+        confirm_button.place(x=165, y=280)
+
+        sign_up_wind.mainloop()
 
     def delete_account(self):
-        os.system("cls")
         i = self.login()
 
         confirmation = input('Are you sure you want to delete your account? (y/n): ')
@@ -186,8 +300,6 @@ class App:
         time.sleep(2)
 
         print('Your changes have been saved!')
-
-    
 
     def start(self):
         i = self.login()
--- frame.py
+++ frame.py
@@ -1,3 +1,4 @@
+from re import I
 import tkinter
 import customtkinter
 
@@ -12,7 +13,7 @@ class TKFrame:
         pass
 
 class CTKFrame:
-    def __init__(self, app_width, app_height, backg, title):
+    def __init__(self, app_width, app_height, backg, title, text):
         customtkinter.set_appearance_mode(backg)
         customtkinter.set_default_color_theme('blue')
 
@@ -22,3 +23,45 @@ class CTKFrame:
 
     def start(self):
         pass
+
+class SuccessWindow:
+    def __init__(self, app_width, app_height, backg, title, line):
+        self.window = tkinter.Tk()
+        self.window.geometry(f'{app_width}x{app_height}')
+        self.window.config(bg=backg)
+        self.window.title(title)
+
+        self.image = tkinter.PhotoImage(file=)
+
+        self.success_label = tkinter.Label(
+            self.window,
+            text=line,
+            bg='black',
+            font=('Courier', 12, 'bold')
+            fg='green',
+            image=self.image
+        )
+
+        self.success_label.place(anchor=tkinter.CENTER)
+        self.window.mainloop()
+
+class ErrorWindow:
+    def __init__(self, app_width, app_height, bgcol, title, line):
+        self.window = tkinter.Tk()
+        self.window.geometry(f'{app_width}x{app_height}')
+        self.window.config(bg=bgcol)
+        self.window.title(title)
+
+        self.image = tkinter.PhotoImage(file=)
+
+        self.success_label = tkinter.Label(
+            self.window,
+            text=line,
+            bg='black',
+            font=('Courier', 12, 'bold')
+            fg='red',
+            image=self.image
+        )
+
+        self.success_label.place(anchor=tkinter.CENTER)
+        self.window.mainloop()
